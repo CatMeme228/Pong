571ca608785c3f8bd82de5d9f696a279f1cf8007
Cat_meme <tolabelaev1251@gmail.com>
2023-11-06 18:25:56 +0300

    Исправлен циклический маршрут мяча, добавлен счёт. Осталось только написать бота!

--- Pong/Pong.cpp
+++ Pong/Pong.cpp
@@ -20,21 +20,22 @@ size_t maxX = 30;
 size_t minX = 0;
 size_t maxY = 9;
 size_t minY = 0;
-size_t Racket0_Ball_positionX = 0;
+size_t Racket0_positionX = 0;
 size_t Racket0_positionY = 5;
 
-int a = 0;
+int FirstPlayerScore = 0;
+int SecondPlayerScore = 0;
 
 void printState() {
     while (true) {
         mtx.lock();
         system("cls");
         for (size_t i = 0; i < 10; ++i) {
-            for (size_t j = 0; j < 30; ++j) {
+            for (size_t j = 0; j < 31; ++j) {
                 if (i == Ball_positionY && j == Ball_positionX) {
                     std::cout << "0";
                 }
-                else if (i == Racket0_positionY && j == Racket0_Ball_positionX) {
+                else if (i == Racket0_positionY && j == Racket0_positionX) {
                     std::cout << "|";
                 }
                 else {
@@ -44,6 +45,9 @@ void printState() {
             }
             std::cout << std::endl;
         }
+        std::cout << std::endl<<"-------------Score------------";
+        std::cout << std::endl<<"       Player     Computer";
+        std::cout << std::endl << "         " << FirstPlayerScore<<"            "<<SecondPlayerScore;
         Ball_positionY += velocityY;
         Ball_positionY = (std::min)(Ball_positionY, maxY);
         Ball_positionY = (std::max)(Ball_positionY, minY);
@@ -52,7 +56,7 @@ void printState() {
         Ball_positionX = (std::max)(Ball_positionX, minX);
         mtx.unlock();
         using namespace std::chrono_literals;
-        std::this_thread::sleep_for(1000ms);
+        std::this_thread::sleep_for(500ms);
 
     }
 }
@@ -62,17 +66,22 @@ void printState() {
 void getState() {
     while (true) {
         mtx.lock();
+        if (Ball_positionY == Racket0_positionY && Ball_positionX - 1 == 0) {
+            velocityX = 1;
+        }
         if (Ball_positionY == 9) {
             velocityY = -1;
         }
         else if (Ball_positionY == 0) {
             velocityY = 1;
         }
-        if (Ball_positionX == 28) {
+        if (Ball_positionX == 30) {
             velocityX = -1;
+            FirstPlayerScore++;
         }
         else if (Ball_positionX == 0) {
             velocityX = 1;
+            SecondPlayerScore++;
         }
         if (GetAsyncKeyState(VK_UP) && Racket0_positionY!= 0) {
             Racket0_positionY--;
@@ -82,7 +91,7 @@ void getState() {
         }
         mtx.unlock();
         using namespace std::chrono_literals;
-        std::this_thread::sleep_for(100ms);
+        std::this_thread::sleep_for(500ms);
     }
 }
 
