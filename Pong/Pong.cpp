a292c6afeb0dd6e0422b713aaa5859453d8fe07c
Cat_meme <tolabelaev1251@gmail.com>
2023-11-09 17:30:27 +0300

    Version 1.0 for 2 players

--- Pong/Pong.cpp
+++ Pong/Pong.cpp
@@ -7,25 +7,27 @@
 
 std::mutex mtx;
 
-int randomNumber(int min, int max) {
-    srand(time(NULL));
-    return min + rand() % (max - min);
-}
-
 int velocityX = 1;
 int velocityY = 1;
+
 size_t Ball_positionX = 16;
 size_t Ball_positionY = 6;
+
 size_t maxX = 30;
 size_t minX = 0;
 size_t maxY = 9;
 size_t minY = 0;
+
 size_t Racket0_positionX = 0;
 size_t Racket0_positionY = 5;
+
+size_t Racket1_positionX = 30;
+size_t Racket1_positionY = 5;
 
 int FirstPlayerScore = 0;
 int SecondPlayerScore = 0;
 
+//Print Field
 void printState() {
     while (true) {
         mtx.lock();
@@ -38,15 +40,18 @@ void printState() {
                 else if (i == Racket0_positionY && j == Racket0_positionX) {
                     std::cout << "|";
                 }
+                else if (i == Racket1_positionY && j == Racket1_positionX) {
+                    std::cout << "|";
+                }
                 else {
-                    std::cout << "#";
+                    std::cout << ".";
                 }
                 
             }
             std::cout << std::endl;
         }
         std::cout << std::endl<<"-------------Score------------";
-        std::cout << std::endl<<"       Player     Computer";
+        std::cout << std::endl<<"     Player 1     Player 2";
         std::cout << std::endl << "         " << FirstPlayerScore<<"            "<<SecondPlayerScore;
         Ball_positionY += velocityY;
         Ball_positionY = (std::min)(Ball_positionY, maxY);
@@ -56,16 +61,18 @@ void printState() {
         Ball_positionX = (std::max)(Ball_positionX, minX);
         mtx.unlock();
         using namespace std::chrono_literals;
-        std::this_thread::sleep_for(300ms);
+        std::this_thread::sleep_for(125ms);
 
     }
 }
 
 
-
+//Moving objects
 void getState() {
     while (true) {
         mtx.lock();
+        
+        //Racket 0
         if (Ball_positionY == Racket0_positionY && Ball_positionX - 1 == 0) {
             velocityX = 1;
         }
@@ -77,12 +84,29 @@ void getState() {
             velocityX = 1;
             velocityY = -1;
         }
+        
+        // Racket 1
+        if (Ball_positionY == Racket1_positionY && Ball_positionX + 1 == 30) {
+            velocityX = -1;
+        }
+        else if (Ball_positionY - 1 == Racket1_positionY && Ball_positionX + 1 == 30) {
+            velocityX = -1;
+            velocityY = 1;
+        }
+        else if (Ball_positionY + 1 == Racket1_positionY && Ball_positionX + 1 == 30) {
+            velocityX = -1;
+            velocityY = -1;
+        }
+        
+        //Walls
         if (Ball_positionY == 9) {
             velocityY = -1;
         }
         else if (Ball_positionY == 0) {
             velocityY = 1;
         }
+
+        //Score
         if (Ball_positionX == 30) {
             velocityX = -1;
             FirstPlayerScore++;
@@ -91,15 +115,24 @@ void getState() {
             velocityX = 1;
             SecondPlayerScore++;
         }
-        if (GetAsyncKeyState(VK_UP) && Racket0_positionY!= 0) {
+
+        //Rackets moves
+        if (GetAsyncKeyState(0x57) && Racket0_positionY!= 0) {
             Racket0_positionY--;
         }
-        else if (GetAsyncKeyState(VK_DOWN) && Racket0_positionY != 9) {
+        else if (GetAsyncKeyState(0x53) && Racket0_positionY != 9) {
             Racket0_positionY++;
+        }
+
+        if (GetAsyncKeyState(VK_UP) && Racket1_positionY != 0) {
+            Racket1_positionY--;
+        }
+        else if (GetAsyncKeyState(VK_DOWN) && Racket1_positionY != 9) {
+            Racket1_positionY++;
         }
         mtx.unlock();
         using namespace std::chrono_literals;
-        std::this_thread::sleep_for(300ms);
+        std::this_thread::sleep_for(125ms);
     }
 }
 
